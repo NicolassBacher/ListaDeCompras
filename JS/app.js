9b018a3ada20294427fd42b17d55416fe2ee1bc7
NicolassBacher <nico.8291@gmail.com>
2022-12-14 12:49:15 -0300

    se agrega local storage

--- JS/app.js
+++ JS/app.js
@@ -1,26 +1,29 @@
 /* Variables BOTONES */
-let btnFormulario = document.getElementById ('btnformulario')
-let btnAgregar = document.getElementById ('btnagregar')
-let btnCerrar1 = document.getElementById ('btncerrar1')
-let btnCerrar2 = document.getElementById ('btncerrar2')
-let btnDetalle = document.getElementById ('btnDetalle')
+let btnFormulario = document.getElementById('btnformulario')
+let btnAgregar = document.getElementById('btnagregar')
+let btnCerrar1 = document.getElementById('btncerrar1')
+let btnCerrar2 = document.getElementById('btncerrar2')
+let btnDetalle = document.getElementById('btnDetalle')
 
 /* variable de datos de formulario */
-let datos = document.getElementById ('datos')
-let enviaDetalle = document.getElementById ('enviaDetalle')
+let datos = document.getElementById('datos')
+let enviaDetalle = document.getElementById('enviaDetalle')
 
 /* Distintas pantallas */
-let formulario = document.getElementById ('formulario')
-let lista = document.getElementById ('lista')
-let inicio = document.getElementById ('inicio')
-var detalle = document.getElementById ('detalle')
-let pantallaOscura = document.getElementById ('pantallaOscura')
+let formulario = document.getElementById('formulario')
+let lista = document.getElementById('lista')
+let inicio = document.getElementById('inicio')
+var detalle = document.getElementById('detalle')
+let pantallaOscura = document.getElementById('pantallaOscura')
 
 /* Contadores */
 let item = 0
 let cantidad = 0
 let bandera = 0
 let identificador = 0
+
+/* ARRAY */
+let guardar = []
 
 
 
@@ -45,7 +48,7 @@ let cerrar = () => {
   btnFormulario.style.display = "inline-block"
   lista.style.opacity = "1"
   inicio.style.opacity = "1"
-  document.getElementById('descripcion').placeholder="Ej: 1 kg. preferentemente verde"
+  document.getElementById('descripcion').placeholder = "Ej: 1 kg. preferentemente verde"
   if (cantidad == 0) {
     inicio.style.display = "flex"
     lista.style.display = "none"
@@ -63,33 +66,13 @@ let abrirDetalle = (titulo, tipo, descripcion, ids) => {
   lista.style.opacity = "0.4"
   pantallaOscura.style.display = "flex"
   let modeloItem
-  if (descripcion === ''){
+  if (descripcion === '') {
     modeloItem = `<img src="${tipo}" alt="" /><div class="detalleItem"><h4>Producto:</h4><p>${titulo}</p></div><div class="detalleItem"><h4>Descripcion:</h4><p style="color: #666;">No agrego ninguna descripcion del Producto</p></div><div class="dosBotones"><button class="boton" onclick="borrar ('${ids}')">Eliminar</button><button class="boton" onclick="modificar ('${titulo}', '${tipo}', '${descripcion}', '${ids}'), abrirFormulario()">Modificar</button></div></div>`
-  }else {
+  } else {
     modeloItem = `<img src="${tipo}" alt="" /><div class="detalleItem"><h4>Producto:</h4><p>${titulo}</p></div><div class="detalleItem"><h4>Descripcion:</h4><p>${descripcion}</p></div><div class="dosBotones"><button class="boton" onclick="borrar ('${ids}')">Eliminar</button><button class="boton" onclick="modificar ('${titulo}', '${tipo}', '${descripcion}', '${ids}'), abrirFormulario()">Modificar</button></div></div>`
   }
-  let padre = document.getElementById ('enviaDetalle')
+  let padre = document.getElementById('enviaDetalle')
   padre.innerHTML = modeloItem
-}
-
-let modificar = (titulo, tipo, descripcion, ids) => {
-  bandera = 1
-  descripcion = descripcion.replace( /<br>/g, '\n')
-  document.getElementById('titulo').value=titulo
-  document.getElementById('tipo').value=tipo
-  document.getElementById('descripcion').value=descripcion
-  document.getElementById('descripcion').placeholder="No agrego ninguna descripcion"
-  btnAgregar.innerHTML = 'Guardar'
-  identificador = ids
-}
-
-let borrar = (ids) => {
-  cantidad -- 
-  if (cantidad < 0){cantidad=0}
-  let itemBorrar = document.getElementById (`${ids}`)
-  itemBorrar.remove ()
-  cerrar ()
-  
 }
 
 /* Funcion para agregar items a la lista */
@@ -98,39 +81,134 @@ let agregarItem = (titulo, tipo, descripcion) => {
   if (bandera == 1) {
     bandera = 0
     let modeloItem1 = `<img src= "${tipo}" alt="" /><h3>${titulo}</h3><button class="boton" onclick="abrirDetalle ('${titulo}', '${tipo}', '${descripcion}', '${identificador}')">Detalle</button>`
-    let padre1 = document.getElementById (`${identificador}`)
-    padre1.innerHTML = modeloItem1 
+    let padre1 = document.getElementById(`${identificador}`)
+    padre1.innerHTML = modeloItem1
+    modificarStorage(titulo, tipo, descripcion, identificador)
     identificador = 0
+
   }
-  else {    
+  else {
     item++
-    cantidad ++
-    let ids = 'id'+ item
+    cantidad++
+    let ids = 'id' + item
     let modeloItem2 = `<li class="item" id="${ids}"><img src= "${tipo}" alt="" /><h3>${titulo}</h3><button class="boton" onclick="abrirDetalle ('${titulo}', '${tipo}', '${descripcion}', '${ids}')">Detalle</button></li>`
-    let padre2 = document.getElementById ('contenedor')
-    padre2.innerHTML += modeloItem2 
-  } 
+    let padre2 = document.getElementById('contenedor')
+    padre2.innerHTML += modeloItem2
+    console.log(titulo)
+    console.log(descripcion)
+    console.log(ids)
+    console.log(tipo)
+    guardarStorage(titulo, tipo, descripcion, ids)
+  }
 }
+
+let modificar = (titulo, tipo, descripcion, ids) => {
+  bandera = 1
+  descripcion = descripcion.replace(/<br>/g, '\n')
+  document.getElementById('titulo').value = titulo
+  document.getElementById('tipo').value = tipo
+  document.getElementById('descripcion').value = descripcion
+  document.getElementById('descripcion').placeholder = "No agrego ninguna descripcion"
+  btnAgregar.innerHTML = 'Guardar'
+  identificador = ids
+}
+
+let borrar = (ids) => {
+  cantidad--
+  if (cantidad < 0) { cantidad = 0 }
+  let itemBorrar = document.getElementById(`${ids}`)
+  itemBorrar.remove()
+  borrarStorage(ids)
+  cerrar()
+
+}
+
+let guardarStorage = (titulo, tipo, descripcion, ids) => {
+  let array = { title: titulo, tipe: tipo, description: descripcion, id: ids }
+  guardar.push(array)
+  localStorage.setItem('items', JSON.stringify(guardar))
+}
+
+let modificarStorage = (titulo, tipo, descripcion, ids) => {
+  guardar.forEach((item) => {
+    if (item.id == ids) {
+      item.title = titulo
+      item.tipe = tipo
+      item.description = descripcion
+    }
+  })
+  localStorage.setItem('items', JSON.stringify(guardar))
+}
+let borrarStorage = (ids) => {
+  for (let i = 0; i < guardar.length; i++) {
+    if (guardar[i].id == ids) {
+      guardar.splice(i, 1)
+    }
+
+  }
+
+  localStorage.setItem('items', JSON.stringify(guardar))
+  console.log(guardar)
+}
+
+
+let verStorage = () => {
+
+  /* investigar promesas porque no llega  acargar las imagenes que esta ejecutando el borrar */
+
+
+  let traerStorage = localStorage.getItem('items')
+  let elementos = {}
+  if (traerStorage) {
+    let promesa = new Promise((resolve, reject) => {
+      elementos = JSON.parse(traerStorage)
+      console.log('parsear')
+      resolve()
+
+
+    })
+    promesa.then(() => {
+      console.log('borrar')
+      localStorage.removeItem('items')
+    })
+      .then(() => console.log(elementos))
+      .then(() => {
+        for (let i = 0; i < elementos.length; i++) {
+          agregarItem(elementos[i].title, elementos[i].description, elementos[i].tipe)
+        }
+      })
+      .then(() => cerrar())
+  }
+
+}
+
+verStorage()
+
+
+
+
+
+
 
 
 
 /* funciona enviar formulario */
-let enviar = (e) => { 
-  e.preventDefault ()
+let enviar = (e) => {
+  e.preventDefault()
   let titulo = e.target.titulo.value
   let tipo = e.target.tipo.value
   let descripcion = e.target.descripcion.value
-  agregarItem (titulo, tipo, descripcion)
-  cerrar ()
+  agregarItem(titulo, tipo, descripcion)
+  cerrar()
 }
 
 /* Llamado de funciones mediante evento clic */
-btnFormulario.addEventListener ('click', abrirFormulario)
-btnCerrar2.addEventListener ('click', cerrar)
-btnCerrar1.addEventListener ('click', cerrar)
+btnFormulario.addEventListener('click', abrirFormulario)
+btnCerrar2.addEventListener('click', cerrar)
+btnCerrar1.addEventListener('click', cerrar)
 
 /* Llamado de funciona para cerrar popup haciendo clicl afuera */
-pantallaOscura.addEventListener ('click', cerrar)
+pantallaOscura.addEventListener('click', cerrar)
 
 /* Llamado de funcion enviar datos */
 datos.addEventListener('submit', enviar)
